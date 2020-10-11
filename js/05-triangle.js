4595f241aed51824c75506cd8c5df06b1f8a4ef0
Chintan012 <60538737+Chintan012@users.noreply.github.com>
2020-10-11 01:27:22 -0700

    Final Commit

--- js/05-triangle.js
+++ js/05-triangle.js
@@ -5,7 +5,7 @@
 let result = "";
 
 for (var i = 0; i < 8; i++) {
-    for (let j = 0; j < i; j++) {
+    for (var j = 0; j < i; j++) {
         result = result.concat("#");
     }
     result = result.concat("\n")
