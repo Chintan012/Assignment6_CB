3b05717ec98fc2aeaaf7f33ff5e07bbc13cb0af5
Chintan012 <60538737+Chintan012@users.noreply.github.com>
2020-10-11 17:27:33 -0700

    Update 05-triangle.js

--- js/05-triangle.js
+++ js/05-triangle.js
@@ -6,9 +6,9 @@ let result = "";
 
 for (var i = 0; i < 8; i++) {
     for (var j = 0; j < i; j++) {
-        result = result.concat("#");
+        result += "#";
     }
-    result = result.concat("\n")
+    result += "\n";
 }
 
 window.console.log(result);
